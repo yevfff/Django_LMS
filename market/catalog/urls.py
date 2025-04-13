c731f367632262f53410fdc0b8318e20e516eb14
Yevfimii <yevfimiiloboda1276@gmail.com>
2025-04-13 16:18:15 +0300

    about me page with bootstrap and tables

--- market/catalog/urls.py
+++ market/catalog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('tailwind/', views.tailwind, name='tailwind'),
     path('register/', views.register, name='register'),
     path('contact/', views.contact_view, name='contact'),
     path('thank-you/', views.thank_you_view, name='thank_you'),
+    path('about_me/', views.about_me, name='about_me'),
 ]
